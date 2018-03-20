5626a492570fcc52bc850bafbaad28f20ac48cea
azelinsky <zelen23@gmail.com>
2018-03-20 15:30:40 +0300

    two panell settings

--- app/src/main/java/com/example/zsoft/calendar_for_nm/preference.java
+++ app/src/main/java/com/example/zsoft/calendar_for_nm/preference.java
@@ -7,6 +7,9 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.List;
 
 /**
  * Created by adolf on 17.03.2018.
@@ -24,20 +27,56 @@ import android.support.annotation.Nullable;
  */
 
 public class preference  extends PreferenceActivity{
+//activity pref
+/*
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().
-                replace(android.R.id.content,new PrefFragment()).commit();
+               replace(android.R.id.content,new PrefFragment()).commit();
+
     }
+
+*/
+
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        super.onBuildHeaders(target);
+        loadHeadersFromResource(R.xml.pref_head,target);
+    }
+
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return  PrefFragment.class.getName().equals(fragmentName);
+    }
+
+
+
     public static class PrefFragment extends PreferenceFragment{
 
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
         }
+
+
+        @Override
+        public void onPause() {
+            super.onPause();
+
+            Log.i("pref__psuse","Pause");
+
+
+
+        }
+
+
     }
+
 
 }
