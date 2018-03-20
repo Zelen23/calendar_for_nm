822bc92d43fc07286193233d68c2960cb26990b1
azelinsky <zelen23@gmail.com>
2018-03-20 11:52:32 +0300

    rewrite_pref_to_fragments

--- app/src/main/java/com/example/zsoft/calendar_for_nm/preference.java
+++ app/src/main/java/com/example/zsoft/calendar_for_nm/preference.java
@@ -5,27 +5,39 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
 /**
  * Created by adolf on 17.03.2018.
+ * раздел настроек по группам
+ * реализовать фрагментами
+ * для маленьких экранов переходом
+ * для больших двумя фрагментами
+ *
+ * 1я группа- стили
+ * -размер
+ * -цвет
+ * -фон
+ * автопименение при выходе из раздела настроек
+ *
  */
 
 public class preference  extends PreferenceActivity{
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-        Preference preference=(Preference)findPreference("customPref");
-        preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                SharedPreferences sharedPreferences=getSharedPreferences("shared",
-                        Activity.MODE_PRIVATE);
 
-                return true;
-            }
-        });
+        getFragmentManager().beginTransaction().
+                replace(android.R.id.content,new PrefFragment()).commit();
+    }
+    public static class PrefFragment extends PreferenceFragment{
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 
 }
